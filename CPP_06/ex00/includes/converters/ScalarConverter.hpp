ff6b3e7f4cb1979ec4cb6cbbbb6d8a8e84ff5f61
joaovictorrosmaninho <joaovictorrosmaninho@gmail.com>
2024-09-02 09:10:29 -0300

    build: CPP06 create files and dirs

---
+++ CPP_06/ex00/includes/converters/ScalarConverter.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joao <joao@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/02 08:43:56 by joao              #+#    #+#             */
+/*   Updated: 2024/09/02 09:09:50 by joao             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifdef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <string.h>
+
+    class ScalarConverter {
+        public:
+            static void convert(std::string& pseudoNumber);
+
+        private:
+    };
+
+#endif
