610373904717ecd33d649be8df518be3f822f5aa
joaovictorrosmaninho <joaovictorrosmaninho@gmail.com>
2024-09-28 16:32:10 -0300

    CPP08: ex02 adding 42 main

--- CPP_08/ex02/main.cpp
+++ CPP_08/ex02/main.cpp
@@ -18,27 +18,62 @@
 
 
 int main( void ) {
+    
+    {
 
-    std::stack<int> std_stack;    
+        std::stack<int> std_stack;    
 
-    MutantStack<int> my_stack;
+        MutantStack<int> my_stack;
 
-    std::cout << "stack is empty: " << my_stack.empty() << std::endl; 
+        std::cout << "stack is empty: " << my_stack.empty() << std::endl; 
 
-    my_stack.push(1);
-    my_stack.push(2);
-    my_stack.push(3);
+        my_stack.push(1);
+        my_stack.push(2);
+        my_stack.push(3);
 
-    std::cout << "stack is empty: " << my_stack.empty() << std::endl; 
+        std::cout << "stack is empty: " << my_stack.empty() << std::endl; 
 
-    std::cout << "iterando na stack" << std::endl;
+        std::cout << "iterando na stack" << std::endl;
 
-    for ( MutantStack<int>::iterator it = my_stack.begin() ; it != my_stack.end(); it++ ) {
-      std::cout <<  "Iterator: " << *it << std::endl;  
+        for ( MutantStack<int>::iterator it = my_stack.begin() ; it != my_stack.end(); it++ ) {
+          std::cout <<  "Iterator: " << *it << std::endl;  
+        }
+
+        std::cout << "ultimo elemento da stack: "  << my_stack.top() << std::endl;
+
+        std::cout << my_stack << std::endl;
+    
     }
-
-    std::cout << "ultimo elemento da stack: "  << my_stack.top() << std::endl;
-
-    std::cout << my_stack << std::endl;
+    {
+        MutantStack<int> mstack;
+        
+        mstack.push(5);
+        mstack.push(17);
+        
+        std::cout << "42 main top: " << mstack.top() << std::endl;
+        
+        mstack.pop();
+        
+        std::cout << "42 main size: " << mstack.size() << std::endl;
+        
+        mstack.push(3);
+        mstack.push(5);
+        mstack.push(737);
+        mstack.push(0);
+        
+        MutantStack<int>::iterator it = mstack.begin();
+        MutantStack<int>::iterator ite = mstack.end();
+       
+        ++it;
+        --it;
+      
+        while (it != ite)
+        {
+          std::cout << "42 main: " << *it << std::endl;
+          ++it;
+        }
+        std::stack<int> s(mstack);
+        return 0;
+    }
 }
 
